721009eb33e619d0dfaf94f8c5d20443ed4653dd
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2023-12-13 17:24:40 +0300

    Rearrange project files

--- CodeGenerator/Form1.cs
+++ CodeGenerator/Form1.cs
@@ -1,17 +1,8 @@
 ﻿using CodeGenerator_Business;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Text;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Code_Generator
 {
